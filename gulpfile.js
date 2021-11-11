946c7bbaa06e78d5dfdb1d6e548ad8c4fdba7d28
Robinnicholas <robinnicholas2000@gmail.com>
2021-11-11 15:05:13 +0530

    new syntax gulp and sass restructured

--- gulpfile.js
+++ gulpfile.js
@@ -3,79 +3,71 @@ const uglify = require('gulp-uglify');
 const sass = require('gulp-sass')(require('sass'));
 const cssmin = require('gulp-cssnano');
 const sourcemaps = require('gulp-sourcemaps'); 
+const autoprefixer = require('gulp-autoprefixer');
+const image = require('gulp-image');
+const htmlmin = require('gulp-htmlmin');
+const { watch, series } = require('gulp');
 
-// const imagemin = require('gulp-imagemin');
+function imageOpti(cb){
+    gulp.src('./src/images/*')
+        .pipe(image())
+        .pipe(gulp.dest('./dist/images'));
+    cb();
+}
+exports.imgop = imageOpti;
 
-// gulp.task('imageMin', async () => {
-//     console.log('compressing images')
-//     gulp.src('./src/images/*')
-//     .pipe(imagemin())
-//     .pipe(gulp.dest('./dist/images'))
-// })
+function htmlMin(cb){
+    gulp.src('./src/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('./dist'));
+    cb();
+}
+exports.minifyHtml = htmlMin;
 
-// log message 
-gulp.task('message', async () => {
-    await console.log('This is message one');
-})
-
-// generatig source map
-gulp.task('sourcemap', async () => {
-    console.log("sourcemap generated...")
-    await gulp.src('./src/sass/*.scss')
+function sourceMap(cb){
+    gulp.src('./src/sass/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./dist/css/'))
-})
+    cb();
+}
+exports.srcmap = sourceMap;
 
-// minify js
-gulp.task('minifyjs', async () =>{
-    console.log("minifying js...")
-    await gulp.src('./src/js/*.js')
+function minifyJs(cb){
+    gulp.src('./src/js/*.js')
         .pipe(uglify())
         .pipe(gulp.dest('./dist/js'));
-})
+    cb();
+}
+exports.minifyjs = minifyJs;
 
-// minify css 
-gulp.task('minifycss', async () =>{
-    console.log("minifying css...")
-    await gulp.src('./src/css/*.css')
+function minifyCss(cb){
+    gulp.src('./src/css/*.css')
         .pipe(cssmin())
+        .pipe(autoprefixer({
+            cascade: false
+        }))
         .pipe(gulp.dest('./dist/css'));
-})
+    cb();
+}
+exports.minifycss = minifyCss;
 
-// compile sass to css 
-gulp.task('sass', async () => {
-    console.log("Sass running...")
-    console.log("compiling css...")
-    await gulp.src('./src/sass/*.scss')
+function sassToCss(cb){
+    gulp.src('./src/sass/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./src/css'));
-})
+    cb();
+}
+exports.sasscomp = sassToCss;
 
-// copy html
-gulp.task('copyHtml', async () => {
-    console.log('Copying html to dist...');
-    await gulp.src('./src/*.html')
-        .pipe(gulp.dest('./dist'))
-})
-// copy images
-gulp.task('copyImg', async () => {
-    console.log('Copying Images to dist...');
-    await gulp.src('./src/images/*.+(png|jpg|svg)')
-        .pipe(gulp.dest('./dist/images'));
-})
+function defaultTask(cb) {
+    watch('./src/*.html' , htmlMin);
+    watch('./src/sass/**/*.scss' , series(sassToCss, sourceMap));
+    watch('./src/css/*.css' , minifyCss);
+    watch('./src/js/*.js' , minifyJs);   
+    watch('./src/images/*' , imageOpti);   
+    cb();
+}
+exports.default = defaultTask;
 
-// watching for changes
-gulp.task('watch', async () => {
-    console.log('Gulp is Watching...');
-    await gulp.watch('./src/*.html', {ignoreInitial: false} ,gulp.series('renderhtml'));
-    await gulp.watch('./src/sass/*.scss', {ignoreInitial: false} ,gulp.series('rendercss'));
-    await gulp.watch('./src/js/*.js', {ignoreInitial: false} ,gulp.series('minifyjs'));
-})
-
-gulp.task('renderhtml', gulp.series('copyHtml', 'copyImg'));
-
-gulp.task('rendercss', gulp.series('sass', 'minifycss' , 'sourcemap'));
-
-gulp.task('default', gulp.series('message', 'copyHtml', 'copyImg', 'sass', 'minifycss', 'minifyjs','watch'));
